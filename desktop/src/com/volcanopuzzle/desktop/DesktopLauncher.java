10f865bb59f3dabe95e5e2a22f41a0bd6d1c8d29
houseofbits <kpudzens@gmail.com>
2018-03-10 09:49:41 +0200

    g

---
+++ desktop/src/com/volcanopuzzle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.volcanopuzzle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.volcanopuzzle.VolcanoPuzzle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VolcanoPuzzle(), config);
+	}
+}
